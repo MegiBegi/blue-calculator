95ba4f180b88a015d4a2646af79b4f1c4d547bad
megibegi <ms.magdalena.kozlowska@gmail.com>
2019-10-29 21:51:02 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from 'react'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
-const App: React.FC = () => {
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Nothing+You+Could+Do&display=swap');
+font-family: 'Nothing You Could Do', cursive;
+  }
+`
+
+const Background = styled.div`
+  width: 1600px;
+  height: 1200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Calculator = styled.div`
+  width: 300px;
+  height: 506px;
+  border-radius: 10px;
+  background-color: #0095b8;
+`
+
+const Window = styled.input`
+  width: 249px;
+  height: 63px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  margin-top: 30px;
+  margin-left: 25px;
+  margin-right: 25px;
+  padding: 0;
+  font-size: 36px;
+  font-family: 'Nothing You Could Do';
+  text-indent: 20px;
+  line-height: 63px;
+`
+
+const App: React.FC = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Background>
+      <GlobalStyles />
+      <Calculator>
+        <Window type='text'></Window>
+      </Calculator>
+    </Background>
+  )
 }
 
-export default App;
+export default App
