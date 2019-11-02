555f4cd9ce669f1b5187154b97a3349bda317e3f
megibegi <ms.magdalena.kozlowska@gmail.com>
2019-11-02 17:43:43 +0100

    small corrections

--- src/Redux/reducers/index.ts
+++ src/Redux/reducers/index.ts
@@ -1,52 +1,39 @@
 import { includes, endsWith } from 'ramda'
-import {
-  Actions,
-  GET_RESULT,
-  PROVIDE_INPUT,
-  USED_PLUS,
-  ZERO,
-} from '../actions'
+import { Actions, GET_RESULT, PROVIDE_INPUT, USED_PLUS, ZERO } from '../actions'
 
 export interface RootState {
   displayed: string
-   
-  zero: boolean
-  
   tooMuch: boolean
-  noSecPAr: boolean
   plusCheck: number
 }
 
 const initialState: RootState = {
   displayed: '',
-  zero: false,
   tooMuch: false,
-  noSecPAr: false,
   plusCheck: 0
 }
 
 const mainReducer = (state: RootState = initialState, action: Actions) => {
   switch (action.type) {
-    
     case PROVIDE_INPUT:
       const currentInput = state.displayed
       const theFinalState =
-        currentInput.length < 11 
+        currentInput.length < 11
           ? { ...state, displayed: state.displayed + action.digit }
           : { ...state, displayed: 'Too much!!!', tooMuch: true }
 
       return theFinalState
 
     case USED_PLUS:
- const theCurrentState = state.displayed
-
-const properScenario = { ...state, 
-  displayed: state.displayed + action.digit,
-  plusCheck: state.plusCheck + 1 
-}
-
-const plusesOverload = endsWith('+', theCurrentState) ? state : properScenario
-
+      const theCurrentState = state.displayed
+      const properScenario = {
+        ...state,
+        displayed: state.displayed + action.digit,
+        plusCheck: state.plusCheck + 1
+      }
+      const plusesOverload = endsWith('+', theCurrentState)
+        ? state
+        : properScenario
       return plusesOverload
 
     case ZERO:
@@ -58,25 +45,19 @@ const plusesOverload = endsWith('+', theCurrentState) ? state : properScenario
             displayed: state.displayed + action.digit,
             zero: true
           }
-      const whatIsDisplayed = state.tooMuch
-        ? { ...state, displayed: 'Too much!!!' }
-        : verifiedState
-      return whatIsDisplayed
+    
+      return verifiedState
 
     case GET_RESULT:
-    const myState = state.displayed
-     const myBetterState = myState + '0' 
-    const doesItEndWithPlus = endsWith('+', myState) ? true : false
-    const whatIsTheState = doesItEndWithPlus ? myBetterState : myState
-    console.log('this is my state' + myState)
-    const result = eval(whatIsTheState)
-    console.log('result:' + result)
-    const displayedResult = result.toString()
-   
+      const myState = state.displayed
+      const myBetterState = myState + '0'
+      const doesItEndWithPlus = endsWith('+', myState) ? true : false
+      const whatIsTheState = doesItEndWithPlus ? myBetterState : myState
+      const result = eval(whatIsTheState)
+      const displayedResult = result.toString()
       return {
         ...state,
         displayed: displayedResult,
-        index: 0,
         plusCheck: 0
       }
 
