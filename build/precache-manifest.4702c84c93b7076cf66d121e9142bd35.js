20c1ef21938632be58d6aa641050878767474ea7
Madalena <madalena@Madalenas-MacBook-Pro.local>
2019-12-25 22:39:49 +0100

    icons

---
+++ build/precache-manifest.4702c84c93b7076cf66d121e9142bd35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647b1594f1fec7ef35f615e9c11ed1b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1bd459a8234d7d93838",
+    "url": "/static/css/2.00762090.chunk.css"
+  },
+  {
+    "revision": "b1bd459a8234d7d93838",
+    "url": "/static/js/2.85fa8650.chunk.js"
+  },
+  {
+    "revision": "857486b084cfe1e335df28bc607f3e17",
+    "url": "/static/js/2.85fa8650.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1a6d8535010288fb691",
+    "url": "/static/js/3.13ccc387.chunk.js"
+  },
+  {
+    "revision": "b275c99c0914e6f20b45",
+    "url": "/static/js/main.815e6295.chunk.js"
+  },
+  {
+    "revision": "08d48b7efa53429bfdca",
+    "url": "/static/js/runtime-main.98c8dcc4.js"
+  }
+]);
